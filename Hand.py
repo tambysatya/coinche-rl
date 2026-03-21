f54925d9ade49ee04b8f91e75455f5d75f9cd364
satya tamby <tambysatya@gmail.com>
2026-03-21 12:33:50 +0100

    refactor

--- Hand.py
+++ Hand.py
@@ -2,6 +2,9 @@ from flax import struct
 import jax
 import jax.numpy as jnp
 import jax.random as rnd
+from jaxtyping import Array, Bool
+
+from Card import *
 
 ACE, TEN, K, Q, J, NINE, EIGHT, SEVEN = 0,1,2,3,4,5,6,7
 to_trump = jnp.array([J,NINE,ACE,TEN,K,Q,EIGHT,SEVEN])
@@ -9,64 +12,33 @@ to_trump = jnp.array([J,NINE,ACE,TEN,K,Q,EIGHT,SEVEN])
 
 key = rnd.key(0)
 
+  
+Hand = Bool [Array, "4 8"]
 
-
-# Special color indices
-ALL_TRUMP=-1
-NO_TRUMP=-2
-
-   
-@struct.dataclass
-class Hand:
-    cards : jax.Array #hand: boolean matrix 4x8 ordered in the color order
-
-@struct.dataclass
-class Card:
-    color : jnp.int32
-    rank : jnp.int32
-
-
-#Reorders the colors (if it was sans at, it becomes trump, and vice versa) 
 @jax.jit
-def setTrump (hand : Hand, color) -> Hand:
-        row = hand.cards[color][to_trump]
-        newhand = hand.cards.at[color].set(row)
-        return Hand(newhand)
+def set_trump (suit : Suit, hand : Hand) -> Hand:
+        """ Reorders the specified suit in the hand """
+        row = hand[suit][to_trump]
+        newhand = hand.at[suit].set(row)
+        return newhand
 
 
-def getColorMask (color) -> jax.Array:
-    """
-        Keeps only the row corresponding to the specified color
-    """
-    mask = jnp.ones([4,8])
-    color = jax.nn.one_hot(color, 4)
-    return color[:,None]*mask
+#Accessors (subhands)
+
+@jax.jit
+def sh_get_suit (suit : Suit, hand : Hand) -> Hand:
+    """ Returns the subhand of cards of the suit """
+    return hand*jax.nn.one_hot(suit,4)[:,None]
+
+@jax.jit
+def sh_higher_in_suit (card : Card, hand : Hand) -> Hand:
+    """ Returns the subhand of cards better than card """
+    mask = jnp.zeros_like(hand, dtype=bool)
+    ranks = jnp.arange(8) < card.rank
+    return hand * mask.at[card.suit].set(ranks)
 
 
+def randomHand ():
+    h = rnd.uniform(key, [4,8])
+    return jnp.bool(jnp.where(h > 0.5, 1, 0))
 
-def betterOnColor (card: Card):
-   """
-        Mask that keeps only the cards of the color above the specified card
-   """
-   mask = jnp.zeros([4,8])
-   cardrank, cardcolor = card.rank, card.color
-   mask = mask.at[cardcolor,:cardrank].set(1)
-   return mask
-
-
-#predicates
-
-
-def hasColor(hand : Hand, color) -> jnp.bool:
-    return jnp.any(hand.cards[color])
-
-def canPlayAbove(hand : Hand, color, rank) -> jnp.bool:
-    return jnp.any(hand[color,:rank])
-
-
-
-def mkHand():
-    hand = rnd.uniform(key, [4,8])
-    hand = jnp.where(hand > 0.5, 1, 0)
-    hand = Hand(hand)
-    return setTrump(hand, 0)
--- Trick.py
+++ Trick.py
@@ -3,50 +3,26 @@ import jax
 import jax.numpy as jnp
 import jax.random as rnd
 
+from Card import *
 from Hand import *
 
 
 
 @struct.dataclass
 class Trick:
-    cards : jnp.array # Players x Color x Rank : bool
-    color : jnp.int32 # Color (first color played)
-    best_card : jnp.array # Color x Rank : bool
-    best_team : jnp.array # Bool (0 for team 1, 1 for team 2)
+    suit : Suit  # Color (first color played)
+    best_card : Card # Color x Rank : bool
+    best_team : jnp.int32 # (0 for team 1, 1 for team 2)
+    cards : Bool [Array, "4 4 8"] # Player x TensorCard
 
 
 
-def  onTrumpTrick (player, hand, trick):
-    trump = trick.color
-    hastrumpP = hasColor(hand, trump)
-    hasbettertrumpP = jnp.any(betterOnColor(trick.best_card)
+def play (trump : Suit, trick : Trick, player, card : Card):
+    pass
 
-def possibleMoves (trump, player, hand, trick):
-    istrumptrick = trick.color == trump # trumps are asked
-    aretrumpplayed = best_card.color == trump # some trump have been played
-    hascolorP = hasColor(hand, trick.color) #the player can play this color
-    hastrumpP = hasColor(hand, trump) # the player has trumps
-    winP = trick.best_team == player % 2 #the team of the player is currently winning the trick
+def new_trick (player, first_card : Card):
+    tensor = card_to_tensor(first_card)
+    cards = jnp.zeros([4,4,8], dtype=bool)
+    cards = cards.at[player].set(tensor)
+    return Trick (first_card.suit, first_card, player % 2, cards)
 
-    condHasColor = hascolorP
-    condHasToOvertrump = (jnp.logical_not(condHasColor) 
-                       & jnp.logical_not(winP) 
-                       & aretrumpplayed
-                       & hastrumpP)
-    condHasToCut = ( jnp.logical_not(condHasColor)
-                   & jnp.logical_not(condHasToOvertrump)
-                   & jnp.logcial_not(winP))
-                  
-
-    colormask = getColorMask(trick.color) 
-    trumpmask = (aretrumpplayed*betterOnColor(trick.best_card)+ #allowed trumps
-                 (1-aretrumpplayed)*getColorMask(trump))
-
-    
-
-
-@jax.jit
-def newTrick (player, card):
-    cards = jnp.zeros([4, 4, 8])
-    cards = cards.at[player].set(card)
-    return Trick (cards, card.color, card, player % 2)
