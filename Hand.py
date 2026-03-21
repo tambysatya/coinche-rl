39a1b36914375f13bc12784bba2db36ca6c45aa8
satya tamby <tambysatya@gmail.com>
2026-03-21 16:41:59 +0100

    hands vectorizations

--- Hand.py
+++ Hand.py
@@ -13,32 +13,44 @@ to_trump = jnp.array([J,NINE,ACE,TEN,K,Q,EIGHT,SEVEN])
 key = rnd.key(0)
 
   
-Hand = Bool [Array, "4 8"]
+Hand = Bool [Array, "B 4 8"]
 
 @jax.jit
-def set_trump (suit : Suit, hand : Hand) -> Hand:
-        """ Reorders the specified suit in the hand """
-        row = hand[suit][to_trump]
-        newhand = hand.at[suit].set(row)
-        return newhand
+def set_trump (suits : Suit, hands : Hand) -> Hand:
+    """ Reorders the specified suit in the hand """
+    def scalar_set_trump (suit : Suit, hand : Hand) -> Hand:
+            row = hand[suit][to_trump]
+            newhand = hand.at[suit].set(row)
+            return newhand
+    return jax.vmap(scalar_set_trump)(suits, hands)
 
 
 #Accessors (subhands)
 
 @jax.jit
-def sh_get_suit (suit : Suit, hand : Hand) -> Hand:
+def sh_get_suit (suits : Suit, hands : Hand) -> Hand:
     """ Returns the subhand of cards of the suit """
-    return hand*jax.nn.one_hot(suit,4)[:,None]
+    def scalar_sh_get_suit (suit : Suit, hand : Hand) -> Hand:
+        return hand*jax.nn.one_hot(suit,4, dtype=bool)[:,None]
+    return jax.vmap(scalar_sh_get_suit)(suits, hands)
 
 @jax.jit
-def sh_higher_in_suit (card : Card, hand : Hand) -> Hand:
+def sh_higher_in_suit (cards : Card, hands : Hand) -> Hand:
     """ Returns the subhand of cards better than card """
-    mask = jnp.zeros_like(hand, dtype=bool)
-    ranks = jnp.arange(8) < card.rank
-    return hand * mask.at[card.suit].set(ranks)
+    def scalar_sh_higher_in_suit (card : Card, hand : Hand) -> Hand:
+        mask = jnp.zeros_like(hand, dtype=bool)
+        ranks = jnp.arange(8) < card.rank
+        return hand * mask.at[card.suit].set(ranks)
+    return jax.vmap(sh_higher_in_suit)(cards, hands)
 
 
-def randomHand ():
+def randomHand (key=key):
     h = rnd.uniform(key, [4,8])
     return jnp.bool(jnp.where(h > 0.5, 1, 0))
+
+def randomHands(keys):
+    n_key = keys.shape[0]
+    h = rnd.uniform(key, [n_key, 4,8])
+    return jnp.bool(jnp.where(h > 0.5, 1, 0))
+
 
