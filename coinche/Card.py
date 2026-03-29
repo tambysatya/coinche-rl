0328719f85b861420f1815b587473ae4a838197a
Satya TAMBY <tambysatya@gmail.com>
2026-03-30 01:30:31 +0200

    trick done: need legal moves

--- coinche/Card.py
+++ coinche/Card.py
@@ -13,7 +13,11 @@ TensorSuit = Bool [Array, "B 4"] # One-hot encoding of a suit
 TensorCard = Bool [Array, "B 12"] #Vectorized representation of a card SUIT(4) + RANK (8)
 
 suit_values = jnp.array([11,10,4,3,2,0,0,0])
-trump_values = jnp.array([20,14,11,10,4,3,2,0,0])
+trump_values = jnp.array([20,14,11,10,4,3,2,0,0])*2/3 #TODO
+no_trump_values = jnp.array([20,10,4,3,2,0,0,0])
+
+SUIT_ALL_TRUMP=5
+SUIT_NO_TRUMP=6
 
 @struct.dataclass
 class Card:
@@ -26,6 +30,11 @@ def card_to_tensor(card : Card) -> TensorCard:
     ranks = jax.nn.one_hot(card.rank, 8, dtype=bool)
     return jnp.concatenate([suits, ranks], axis=-1)
 
+def dummy_card() -> Card:
+    """ Creates a dummy card (encoded as [False....False])
+        Aims to describe an unknown card"""
+    return Card(jnp.array([-1])[:,None], jnp.array([-1])[:,None])
+
 @jax.jit
 def card_from_tensor(ts : TensorCard) -> Card:
     suits, ranks = ts[:, :4], ts[:, 4:]
@@ -34,7 +43,7 @@ def card_from_tensor(ts : TensorCard) -> Card:
 @jax.jit
 def card_to_subhand (card : Card) -> Bool [Array, "B 4 8"]:
     idx = card.suit*8 + card.rank
-    return jax.nn.one_hot(idx,32).reshape(-1, 4, 8)
+    return jax.nn.one_hot(idx,32, dtype=bool).reshape(-1, 4, 8)
 
 
 def show_card(trump, card : Card, index=0) -> str:
@@ -54,9 +63,12 @@ def show_card(trump, card : Card, index=0) -> str:
 
 @jax.jit
 def card_value (trump : Suit, card : Card) -> str:
-    return jnp.where(trump == card.suit,
+    """ Crashes if the card is invalid """
+    return jnp.where((trump == card.suit) | (trump == SUIT_ALL_TRUMP),
                      trump_values[card.rank],
-                     suit_values[card.rank])
+                     jnp.where(trump == SUIT_NO_TRUMP,
+                               no_trump_values[card.rank],
+                               suit_values[card.rank]))
 
 @jax.jit
 def is_better_p (trump : Suit, cardA : Card, cardB : Card) -> jnp.bool:
@@ -64,5 +76,6 @@ def is_better_p (trump : Suit, cardA : Card, cardB : Card) -> jnp.bool:
    same_color_p = cardA.suit == cardB.suit
    cardb_trump_p = trump == cardB.suit
    return jnp.bool(same_color_p*(cardA.rank < cardB.rank) | jnp.logical_not(cardb_trump_p))
+
 
  
