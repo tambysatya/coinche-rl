03689fe0079372755f061f183ff71b9698180907
Satya TAMBY <tambysatya@gmail.com>
2026-04-04 12:19:00 +0200

    bid state

--- coinche/Bid.py
+++ coinche/Bid.py
@@ -2,6 +2,7 @@ from flax import struct
 import jax
 import jax.numpy as jnp
 import jax.random as rnd
+import jax.tree_util as jtu
 from jaxtyping import Array, Bool, Int
 
 
@@ -10,44 +11,61 @@ TensorBid = Bool [Array, "B 62"] # 60 + Coinche + Overcoinche
 
 @struct.dataclass
 class Bid:
-    bid : Bool [Array, "B 6 10"]
+    """ A bid made by a player """
+    suit : Int [Array, "B"] # [0,5] total 6: all 4 suits + ALL_TRUMP + NO_TRUMP 
+    rank : Int [Array, "B"] #0-8 total 9: from 80 to 160 (8 calls) + ALL_IN 
+    check : Bool [Array, "B"]
     coincheP : Bool [Array, "B"]
     overcoincheP : Bool [Array, "B"]
 
-@jax.jit
-def bid_to_tensor(bid : Bid) -> TensorBid:
-    tensor = bid.bid.reshape(-1, 60)
-    return jnp.concatenate([tensor, bid.coincheP, bid.overcoincheP], axis=1)
 
-@jax.jit
-def bid_from_tensor(tensor: TensorBid) -> Bid:
-    bid, coincheP, overcoincheP = tensor[:, :60], tensor[:, 60], tensor[:, 61]
-    return Bid(bid.reshape(-1, 6, 10), coincheP, overcoincheP)
+@struct.dataclass
+class BidMask:
+    """ Masks on the possible bids """
+    possible_rank : Bool [Array, "B 6"]
+    possible_check : Bool [Array, "B"]
+    possible_coinche : Bool [Array, "B"]
+    possible_overcoinche : Bool [Array, "B"]
 
-@jax.jit
-def trump_from_bid(bid : Bid):
-    return bid.bid.sum(axis=1).argmax(axis=-1)
+
 
 
 @jax.jit
-def possible_bid_mask (tensor : TensorBid) -> TensorBid:
-    """ Generates the possible bidding actions (i.e. coincheP = 1 => you are allowed to coinche)"""
-    batch_size = tensor.shape[0]
+def bid_to_tensor (bid : Bid):
+    return jnp.concatenate([jax.nn.one_hot(bid.suit, 6),
+                            jax.nn.one_hot(bid.rank, 9),
+                            bid.check[:,None],
+                            bid.coincheP[:,None],
+                            bid.overcoincheP[:,None]], axis=1)
 
-    bid = bid_from_tensor(tensor)
-    coinche_tensor = jnp.ones([batch_size], dtype=bool).reshape(-1,1)
-    overcoinche_tensor = jnp.ones([batch_size], dtype=bool).reshape(-1,1)
+@jax.jit
+def bids_above (bid : Bid) -> BidMask: 
+    """ Given a batch of bid, gives a mask of all the allowed bids """
+    
+    batch_size = bid.suit.shape[0]
 
-    all_ranks = jnp.arange(10).reshape(1,1,10).repeat(6, axis=1).repeat(batch_size, axis=0)
-    current_rank = bid.bid.sum(axis=1).argmax(axis=1)
-    above_bid = jnp.where(all_ranks > current_rank, True, False)
+    emptyval = jnp.array([-1])
+    true = jnp.ones([batch_size], dtype=bool)
+    false = jnp.zeros([batch_size], dtype=bool)
+
+
+
+    # if the player can raise, he can also check or coinche
+    raised_mask = BidMask (jnp.tile(jnp.arange(0, 9),(batch_size,1)) > bid.rank,
+                      true, true, false) #
+    # if the game has been coinched, he can either overcoinche or check
+    coinched_mask = BidMask (jnp.zeros([batch_size, 9], dtype=bool),true, false, true) #if coinched, you can only overcoinche
+
+    def new_bid(raised, coinched):
+        return jnp.where(
+                    bid.coincheP,
+                    coinched,
+                    raised)
+
+    return jtu.tree_map (new_bid, raised_bid, coinched_bid)
 
     
 
-    above_bid_mask = Bid((1-bid.coincheP)*above_bid,
-                     (1-bid.coincheP)*coinche_tensor, 
-                     overcoinche_tensor)
-    above_bid_mask = bid_to_tensor(above_bid_mask)
-    return above_bid_mask
-    
+
+   
 
